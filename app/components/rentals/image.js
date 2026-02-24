f4a3f1bea5217892af830ab8452f70d4f606f7be
Miles Falk <milesfalk@gmail.com>
2026-02-23 16:22:05 -0800

    Update image.js
    $

--- app/components/rentals/image.js
+++ app/components/rentals/image.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { on } from '@ember/modifier';  
 
 export default class RentalsImage extends Component {
 	@tracked isLarge = false;
@@ -8,4 +9,18 @@ export default class RentalsImage extends Component {
 	@action toggleSize() {
     	this.isLarge = !this.isLarge;
 	}
+
+	<template>
+ 		{{#if this.isLarge}}
+	  		<button type="button" class="image large" {{on "click" this.toggleSize}}>
+    			<img ...attributes>
+    			<small>View Smaller</small>
+  			</button>
+ 		{{else}}
+  			<button type="button" class="image" {{on "click" this.toggleSize}}>
+    			<img ...attributes>
+    			<small>View Larger</small>
+  			</button>
+ 		{{/if}}
+	</template>
 }
