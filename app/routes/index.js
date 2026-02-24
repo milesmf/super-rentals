5a71490fb55ac41842a588fe5f7f9bbc349d38fe
Miles Falk <milesfalk@gmail.com>
2026-02-24 15:50:58 -0800

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,11 @@
 import Route from '@ember/routing/route';
 
+const COMMUNITY_CATEGORIES = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
 export default class IndexRoute extends Route {
 	async model() {
     	let response = await fetch('/api/rentals.json');
