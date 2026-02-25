681ac441ca7bb8603042b4c5582b345b68a5a9e7
Miles Falk <milesfalk@gmail.com>
2026-02-25 11:15:48 -0800

    Update rental.js

--- app/utils/rental.js
+++ app/utils/rental.js
@@ -1,7 +1,8 @@
 const COMMUNITY_CATEGORIES = ["Condo", "Townhouse', "Apartment"];
 
-export function transformental id, attributes }) {
-let type = COMMUNITY_CATEGORIES.includes(attributes.category)
-? 'Community'
-: 'Standalone';
-return
+export function transformRental({ id, attributes }) {
+	let type = COMMUNITY_CATEGORIES.includes(attributes.category)
+		? 'Community'
+		: 'Standalone';
+	return { id, type, ...attributes};
+}
