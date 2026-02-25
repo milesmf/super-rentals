1029b3b42c7931c51be72eb9a2e77a3ebd4e94ea
Miles Falk <milesfalk@gmail.com>
2026-02-25 11:11:59 -0800

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ import { transformRental } from "../utils/rental';
 export default class IndexRoute extends Route {
 	async model {
 		let response = await fetch('/api/rentals.json');
-		let { data } = await response.jsonO;
+		let { data } = await response.json();
 		return data.map(transformRental);
 	}
 }
