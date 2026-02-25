79974a8b719f1ae59f320a363619cdbf439dda87
milesmf <milesfalk@gmail.com>
2026-02-25 19:07:55 +0000

    $

---
+++ app/utils/rental.js
@@ -0,0 +1,7 @@
+const COMMUNITY_CATEGORIES = ['Condo', "Townhouse', "Apartment'];
+
+export function transformental id, attributes }) {
+let type = COMMUNITY_CATEGORIES.includes(attributes.category)
+? 'Community'
+: 'Standalone';
+return
